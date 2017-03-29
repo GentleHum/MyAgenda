056cc0be964a8c837e52cff606006f6fbbf87a89
Mike Vork <mwvork@gmail.com>
2017-03-28 22:25:26 -0500

    Added Long Press to delete feature on All Items screen.

--- MyAgenda/AllItemsTableViewController.swift
+++ MyAgenda/AllItemsTableViewController.swift
@@ -36,6 +36,45 @@ class AllItemsTableViewController: UITableViewController {
 
         // no title on the navigation back button
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        // add gesture recognizers
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        longPressGesture.minimumPressDuration = 0.75;
+        tableView.gestureRecognizers = [longPressGesture]
+
+    }
+    
+    private func delete(from view: UITableView, at indexPath: IndexPath) {
+        let itemToDelete = agendaItems[indexPath.row]
+        ModelController.sharedInstance.deleteAgendaItem(itemToDelete)
+        agendaItems.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        tableView.reloadData()
+    }
+    
+    private func showDeleteConfirmAlert(forCellAt indexPath: IndexPath) {
+        let itemDescription = agendaItems[indexPath.row].descriptionText ?? ""
+        let message = "Delete '" + itemDescription + "'?"
+        let alert: UIAlertController = UIAlertController(title: "Please Confirm",
+                                                         message: message,
+                                                         preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { (UIAlertAction) -> Void in
+            self.delete(from: self.tableView, at: indexPath)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil);
+    }
+
+    func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        if recognizer.state == .began {
+            let point = recognizer.location(in: tableView)
+            if let indexPath = tableView.indexPathForRow(at: point) {
+                showDeleteConfirmAlert(forCellAt: indexPath)
+            }
+        }
     }
     
     
@@ -44,6 +83,7 @@ class AllItemsTableViewController: UITableViewController {
         agendaItems = ModelController.sharedInstance.loadAgendaItems()
         tableView.reloadData()
     }
+    
 
     // MARK: - Table view data source
 
@@ -60,7 +100,7 @@ class AllItemsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.cellIdentifier, for: indexPath) as! AgendaItemTVC
-
+        
         // Configure the cell...
         cell.agendaItem = agendaItems[indexPath.row]
 
@@ -69,10 +109,11 @@ class AllItemsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let itemToDelete = agendaItems[indexPath.row]
-            ModelController.sharedInstance.deleteAgendaItem(itemToDelete)
-            agendaItems.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            delete(from: tableView, at: indexPath)
+//            let itemToDelete = agendaItems[indexPath.row]
+//            ModelController.sharedInstance.deleteAgendaItem(itemToDelete)
+//            agendaItems.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
