e8892650a91305af18fe0a1948e786a0777e5702
Mike Vork <mwvork@gmail.com>
2017-04-02 10:38:56 -0400

    Added the Date Picker so due dates can be selected.  Still needs some layout work but the functionality is in place.

--- MyAgenda/AddAgendaItemViewController.swift
+++ MyAgenda/AddAgendaItemViewController.swift
@@ -13,17 +13,22 @@ class AddAgendaItemViewController: UIViewController {
     @IBOutlet weak var descriptionField: UITextField!
     @IBOutlet weak var priorityChoice: UISegmentedControl!
     @IBOutlet weak var categoryField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.navigationItem.title = "Add to Agenda"
+        
+        dueDatePicker.tintColor = .darkGray
+        dueDatePicker.sizeThatFits(CGSize(width: 200, height: 100))
 
     }
 
     @IBAction func saveWasPressed(_ sender: Any) {
         let priority = priorityChoice.selectedSegmentIndex + 1 // adjust from zero-based
+        let dueDate = dueDatePicker.date
         let descriptionText = descriptionField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
         let category = categoryField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
         if descriptionText != "" && category != "" {
@@ -32,11 +37,12 @@ class AddAgendaItemViewController: UIViewController {
             _ = model.addAgendaItem(descriptionText: descriptionText,
                                     category: category,
                                     priority: priority,
-                                    dueDate: Date())
+                                    dueDate: dueDate)
             
             // and return to the previous controller
             _ = navigationController?.popViewController(animated: true)
         }
+        
     }
     
 }
