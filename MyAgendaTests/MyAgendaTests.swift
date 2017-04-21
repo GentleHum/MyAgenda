9007bd2eb5e6df4b71ea3a13cc8f6746dc14194e
Mike Vork <mwvork@gmail.com>
2017-04-21 10:45:05 -0500

    Added some test files

---
+++ MyAgendaTests/MyAgendaTests.swift
@@ -0,0 +1,25 @@
+//
+//  MyAgendaTests.swift
+//  MyAgendaTests
+//
+//  Created by Mike Vork on 3/20/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import XCTest
+@testable import MyAgenda
+
+class MyAgendaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+}
